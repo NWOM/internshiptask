66c27202640ebd4c265bb9aaaf1d971385046dcc
NWOM <98202572+NWOM@users.noreply.github.com>
2024-08-18 16:43:02 +0530

    Update main.jsx

--- Test1/Q3/formvalidation/src/main.jsx
+++ Test1/Q3/formvalidation/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'; // Correct import for React 18
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-// Create the root element using createRoot from 'react-dom/client'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
